0b07d1bd34476ba88d92a4f3a61c5d188673a332
AnimateBow4809 <alidashtbozorg709@gmail.com>
2024-05-06 08:01:38 +0330

    added commented files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,17 +8,17 @@ public class Main {
         return a+b;
     }
 
-//    public static int mul2(int a,int b){
-//        int ans=0;
-//        for (int i = 0; i < b; i++) {
-//            ans =add(a,ans);
-//        }
-//        return ans;
-//    }
-//
-//    public static int div(int a,int b){
-//        return a/b;
-//    }
+    public static int mul2(int a,int b){
+        int ans=0;
+        for (int i = 0; i < b; i++) {
+            ans =add(a,ans);
+        }
+        return ans;
+    }
+
+    public static int div(int a,int b){
+        return a/b;
+    }
 
 
     public static int mul(int a,int b){
